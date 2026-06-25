b380e83e4595ea24922590993c6b334ba4a38ae5
AnandShah10 <shahanand1072004@gmail.com>
2026-06-25 12:39:11 +0530

    fix

--- exams/views.py
+++ exams/views.py
@@ -606,7 +606,7 @@ class ExamSubmitView(APIView):
                 return Response({'submitted': True, 'message': 'Answers submitted. Results will be released by the faculty.'})
         else:
             from results.models import MarkSheet
-            MarkSheet.objects.get_or_create(exam=session.exam, student=session.student)
+            MarkSheet.objects.get_or_create(exam=session.exam, student=session.student,paper_checker=session.exam.paper_checker)
             return Response({'submitted': True, 'message': 'Answers submitted. Results pending review.'})
 
 
