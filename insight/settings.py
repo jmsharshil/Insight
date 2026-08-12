2eef69e8be064398e97f11a9e9fbadb2d78973fc
Hemish123 <consultant@jmsadvisory.in>
2026-08-12 15:57:29 +0530

    credentials change

--- insight/settings.py
+++ insight/settings.py
@@ -208,11 +208,11 @@ AUTH_USER_MODEL = 'auth_user.User'
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'iips.insightinstitute@gmail.com'
-EMAIL_HOST_PASSWORD = 'fixn oprh zxpz uwor'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 BASE_URL = os.getenv('BASE_URL', 'http://localhost:8000')
 FRONTEND_BASE_URL = os.getenv('FRONTEND_BASE_URL', 'http://localhost:5173')
